ec9f1e6195f7590691f117825094d3c1c759fdfd
tusharshaha <tusharshaha128@gmail.com>
2021-10-04 21:44:16 +0600

    fixed scrollbar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -20,7 +20,7 @@ const Home = () => {
                     <p className='text-secondary mt-3'> The consistent practice has shown to increase overall mobility, improve your body's pressure response, and increase muscle. Because so many martial arts involve repeat muscular actions over time you'll build strength and burn fat.</p>
                 </Col>
                 <Col md={5}>
-                <Image src={img1} rounded/>
+                <Image src={img1} fluid/>
                 </Col>
                 </Row>
                 <div className='text-center my-5'>
