684225f088d8cf4c62cd94db9a970fba0da3dfe7
tusharshaha <tusharshaha128@gmail.com>
2021-10-04 11:28:45 +0600

    file ready

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css'
+const Header = () => {
+    return (
+        <div className='header-section'>
+            <div className='overlay'>
+                <h3 className='bg-warning'>hello world</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
