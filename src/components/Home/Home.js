d909726e3b60d177a54b66aeb942b1fbc7162c71
tusharshaha <tusharshaha128@gmail.com>
2021-10-05 18:56:01 +0600

    final test

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,13 +12,12 @@ const Home = () => {
             <div className='overlay'>
             <Container className='text-white'>
                 <Row className='mt-5'>
-                    <Col>
+                    <Col xs={10}md={7} lg={6}>
                     <h1 className='fw-bold'>Martial Arts</h1>
                     <h3 className='text-uppercase bg-danger px-3'>Traning With best legend</h3>
                     <p>Every day we bring hope to millions of chidren in the world's hardes places as a sing of God's unconditonal love</p>
                     <Button variant="danger mt-3 ">BOOK NOW</Button>
                     </Col>
-                    <Col></Col>
                 </Row>
             </Container>
         </div>
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { Container , Button } from 'react-bootstrap';
-import './signin.css'
-
+import { Container , Button, Row, Col, Form } from 'react-bootstrap';
 const SignIn = () => {
     return (
         <Container>
-            <div className='text-center w-50 mx-auto signIn'>
-                <h3 className='fw-bold'>Sign In</h3>
-               <input className='input' type="email" placeholder='Your Email' /><br/>
-              <input className='input' type="password" name="" id=""  placeholder='Your Password'/> <br />
-              <Button variant="outline-secondary mt-3">Submit</Button>
-            </div>
-        </Container>
+        <h1 className="shadow-sm text-success mt-5 p-3 text-center rounded">Sign In</h1>
+        <Row className="mt-5">
+            <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
+                <Form >
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Button variant="outline-success mt-4 btn-block" type="">
+                        SUBMIT
+                    </Button>
+                </Form>
+            </Col>
+        </Row>
+    </Container>
     );
 };
 
